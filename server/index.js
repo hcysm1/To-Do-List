62dae9ce95c46d98d5d8922ae7417e122d27e8ee
Sonia Mubasher <soniamubasher1991@gmail.com>
2024-07-10 20:40:49 +0800

    updated server file

--- server/index.js
+++ server/index.js
@@ -4,12 +4,20 @@ import cors from "cors";
 import route from "./routes/route.js";
 
 const app = express();
-app.use(cors());
+
+// CORS configuration
+const corsOptions = {
+  origin: "*", // Allow requests from all domains.
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", route);
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 connection();
 app.listen(PORT, () =>
   console.log(`your server is running successfully on PORT ${PORT}`)
